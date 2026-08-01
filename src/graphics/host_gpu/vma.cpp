efdcc30a16d493caec7bd3325a3a19a449f6404a
Your Name <you@example.com>
2026-08-01 15:02:53 -0700

    fix: move GetBufferDeviceAddress out of header to vma.cpp to fix CI build errors

--- src/graphics/host_gpu/vma.cpp
+++ src/graphics/host_gpu/vma.cpp
@@ -350,4 +350,11 @@ void GraphicContext::LoadHardwareRayTracingFunctions() {
 	}
 }
 
+vk::DeviceAddress GraphicContext::GetBufferDeviceAddress(const VulkanBuffer& buffer) const {
+	if (!buffer.buffer) { return 0; }
+	vk::BufferDeviceAddressInfo info{};
+	info.buffer = buffer.buffer;
+	return device.getBufferAddress(&info);
+}
+
 } // namespace Libs::Graphics
