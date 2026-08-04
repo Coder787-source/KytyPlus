bdf910cd1459b0064003c1502b26a35e21f3e54c
Your Name <you@example.com>
2026-08-03 17:34:41 -0700

    Remove junk files (zips, logs, temp scripts, markdown) - preserve all .hpp/.h/.cpp

--- _configure_launcher.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-if errorlevel 1 exit /b 1
-cmake -S src -B _BuildLauncher -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang-cl -DCMAKE_CXX_COMPILER=clang-cl -DKYTY_BUILD_LAUNCHER=ON -DCMAKE_PREFIX_PATH="C:\Qt\6.10.3\msvc2022_64"
